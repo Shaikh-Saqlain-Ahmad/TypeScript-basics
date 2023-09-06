26911c766075cd016346460380395aca8fccd836
Saqlain Ahmad <saqlainahmed292@gmail.com>
2023-09-06 18:45:16 +0500

    js file updated

--- practice.js
+++ practice.js
@@ -8,4 +8,11 @@ console.log(number);
 number = 39; //evolved into int
 number.toFixed();
 console.log(number);
-var age = 'sixty';
+var age = 'sixty'; //type annotations
+var lifespan;
+lifespan = 89; // Ok
+lifespan = "ongoing"; // Ok
+var someString = "";
+someString = ":)";
+var mathematician;
+console.log(mathematician === null || mathematician === void 0 ? void 0 : mathematician.length);
