4887b39d3ec371b5a89e60b9545697d8f1f20717
Saqlain Ahmad <saqlainahmed292@gmail.com>
2023-09-06 16:28:46 +0500

    any type and type annotations

--- practice.js
+++ practice.js
@@ -1,1 +1,11 @@
 console.log('hello');
+var best_player = Math.random() > 4 ? 'Alex' : 'Max';
+console.log(best_player);
+var number; // type any
+number = 'thirty'; // evolved into string
+number.toUpperCase();
+console.log(number);
+number = 39; //evolved into int
+number.toFixed();
+console.log(number);
+var age = 'sixty';
--- practice.ts
+++ practice.ts
@@ -1,4 +1,14 @@
 console.log('hello');
 let best_player=Math.random()>4?'Alex':'Max';
 console.log(best_player);
+let number;// type any
+number='thirty';// evolved into string
+number.toUpperCase();
+console.log(number);
+number=39;//evolved into int
+number.toFixed();
+console.log(number);
+let age:string='sixty';//type annotations
+
+
 
