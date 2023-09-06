9f59884f4c93131377b051f9594c67adf7c5d01e
Saqlain Ahmad <saqlainahmed292@gmail.com>
2023-09-06 18:43:42 +0500

    unions and literals usage

--- practice.ts
+++ practice.ts
@@ -9,6 +9,14 @@ number=39;//evolved into int
 number.toFixed();
 console.log(number);
 let age:string='sixty';//type annotations
+let lifespan: number | "ongoing" | "uncertain";
+lifespan = 89; // Ok
+lifespan = "ongoing"; // Ok
+let someString = "";
+someString = ":)";
+let mathematician: string|undefined;
+console.log(mathematician?.length);
+
 
 
 
